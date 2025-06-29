a606aa2c0b2566877c672f43f54e957bdbc59100
camara1993 <47604758+camara1993@users.noreply.github.com>
2025-06-29 01:02:49 +0000

    z

--- backend/src/main/java/com/esp/esp_diploma_bon/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.esp.esp_diploma_bon.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
--- esp-diplomes/src/services/admin.service.js
+++
@@ -1,9 +0,0 @@
-export const adminService = {
-    getDashboardData: async () => {
-      // Implémenter selon vos besoins
-      return Promise.resolve({
-        // Données de tableau de bord admin
-      });
-    },
-    // Autres méthodes pour l'administration
-  };
