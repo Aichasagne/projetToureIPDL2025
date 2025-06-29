e2038a0c5f901b3c47bae4d12707ccc985888624
camara1993 <47604758+camara1993@users.noreply.github.com>
2025-06-29 00:52:32 +0000

    c

--- backend/src/main/java/com/esp/esp_diplomas/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.esp.esp_diplomas.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
--- backend/src/main/java/com/esp/esp_diplomas/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.esp.esp_diplomas.repository;
-
-import com.esp.esp_diplomas.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
